b9527b41095627a4da678fbbaa06cd69f3e73de6
sivaganesh07 <02sivaganesh@gmail.com>
2025-02-03 16:04:46 -0800

    V1

---
+++ pdf-rag.py
@@ -0,0 +1,7 @@
+## 1. Ingest PDF Files
+# 2. Extract Text from PDF Files and split into small chunks
+# 3. Send the chunks to the embedding model
+# 4. Save the embeddings to a vector database
+# 5. Perform similarity search on the vector database to find similar documents
+# 6. retrieve the similar documents and present them to the user
+## run pip install -r requirements.txt to install the required packages
